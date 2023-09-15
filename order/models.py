2ea23710597f6b3b0acbd872ba490bbcd6629249
sajjadp96 <sajjad.piri1997@gmail.com>
2023-09-15 16:43:07 +0330

    core: order model and orderitems model added

--- order/models.py
+++ order/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from user.models import Address
+from product.models import Product
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+    
+class Order(BaseModel):
+    customer = models.CharField(max_length=15,)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+
+    @property
+    def price(self):
+        return sum([item.price*item.quantity for item in self.orderitem_set.all()])
+
+    def __str__(self) -> str:
+        return f"{self.customer}"
+
+
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    
+
+    def __str__(self) -> str:
+        return f"{self.quantity}"
