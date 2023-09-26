ab74db66736ff27a849f24dcfd6647c2a1695485
sajjadp96 <sajjad.piri1997@gmail.com>
2023-09-26 19:36:41 +0330

    Categoryl ist  and product listadded to views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import Category,Product
+from .serializers import CategorySerializer,ProductSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class CategoryList(APIView):
+    """
+    List all Category.
+    """
+    def get(self, request, format=None):
+        categorise = Category.objects.all()
+        serializer = CategorySerializer(categorise, many=True)
+        return Response(serializer.data)
+
+class ProductList(APIView):
+    """
+    List all Category.
+    """
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
