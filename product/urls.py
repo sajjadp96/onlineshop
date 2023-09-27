c52963b7ee38b6520b26847d064d5dacd3200ec6
sajjadp96 <sajjad.piri1997@gmail.com>
2023-09-27 19:03:08 +0330

    search defiend in urls

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from product.views import CategoryList,ProductList
+from product.views import CategoryList,ProductList,SearchView
 
 urlpatterns = [
     path('categories/', CategoryList.as_view(),name='categories'),
-    path('products/', ProductList.as_view(),name='products'),    
+    path('products/', ProductList.as_view(),name='products'),
+    path('search/',SearchView.as_view(),name='search')    
 ]
